6c66e9f8ea18835813480aca34a909eb330d0397
Ashton Knoblock <Ashton.knoblock@gmail.com>
2018-05-03 14:16:22 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongodb = require('mongodb')
+
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://ds113640.mlab.com:13640/tester');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('database connected')
+})
+
+app.get('/', (req, res) => { 
+    res.send('This is a game');
+});
+
+
+
+
+
+
+app.listen(3000, () => console.log( "youre connected to http://localhost:3000" ));
+
+
+
